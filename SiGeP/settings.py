6300b5dc71467d7401ea3ffdf6d7c2667a494b81
federicopeker <federicopeker@gmail.com>
2013-07-11 13:58:02 -0700

    Creado los modulos de ComponentesDePedido, RecursosDeEmpresa y PedidoRegistrado

--- SiGeP/settings.py
+++ SiGeP/settings.py
@@ -123,6 +123,9 @@ INSTALLED_APPS = (
      'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
      'django.contrib.admindocs',
+     'ComponentesDePedido'
+     'RecursosDeEmpresa'
+     
 )
 
 # A sample logging configuration. The only tangible logging
