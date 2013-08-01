f0a7d751d65c9100039b48a5fdcb4ec49db61425
Federico Peker <federicopeker@gmail.com>
2013-08-01 14:01:56 -0700

    [#5]continuamos con la pagina baseAdmin.html

--- RecursosDeEmpresa/views.py
+++ RecursosDeEmpresa/views.py
@@ -1,1 +1,19 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from RecursosDeEmpresa.models import TipoDocumento
+# Paginacion en Django
+from django.core.paginator import Paginator,EmptyPage,InvalidPage
+
+def tipoDocumento_view(request):
+    tipos = TipoDocumento.objects.all() # Select * from ventas_productos where status = True
+#    paginator = Paginator(lista_tipo,5) # Cuantos productos quieres por pagina? = 3
+#    try:
+#        page = int(pagina)
+#    except:
+#        page = 1
+#    try:
+#        tipos = paginator.page(page)
+#    except (EmptyPage,InvalidPage):
+#        tipos = paginator.page(paginator.num_pages)
+    ctx = {'tipos':tipos}
+    return render_to_response('RecursosDeEmpresa/tipoDocumento.html',ctx,context_instance=RequestContext(request))# Create your views here.
