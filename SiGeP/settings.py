ebbd003ec9225ef116c3bc76b61a3bcf0e9cda15
Federico Peker <federicopeker@gmail.com>
2013-08-12 15:44:30 -0700

    [#7][#8] modifique el change_form.html para que muestre en español

--- SiGeP/settings.py
+++ SiGeP/settings.py
@@ -16,7 +16,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'D:\\Proyectos\\Sigep\\trunk\\SiGeP\\sqlite.db',                      # Or path to database file if using sqlite3.
+        'NAME': 'D:\\Proyecto Final\\Sigep\\trunk\\SiGeP\\sqlite.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -117,10 +117,11 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.sites',
+#    'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
+    'django_admin_bootstrapped',
      'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
      'django.contrib.admindocs',
