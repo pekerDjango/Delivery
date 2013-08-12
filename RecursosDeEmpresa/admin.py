076202f7eec5c9e5f82f91ef3a48e0f2fad1bb13
Federico Peker <federicopeker@gmail.com>
2013-08-12 15:38:46 -0700

    [#8] campo busqueda en empleado

--- RecursosDeEmpresa/admin.py
+++ RecursosDeEmpresa/admin.py
@@ -1,7 +1,23 @@
 from RecursosDeEmpresa.models import Empleado, Sucursal, TipoDocumento, Barrio, Localidad, TelefonoPersona, TelefonoSucursal, Turno, CalificacionServicio, Provincia 
 from django.contrib import admin
-admin.site.register(Empleado)
-admin.site.register(Sucursal)
+from django_admin_bootstrapped.admin.models import SortableInline
+
+class TelefonoSucursalInLine(admin.StackedInline, SortableInline):
+    model = TelefonoSucursal
+    verbose_name_plural = "Telefonos de Sucursales"
+    extra = 1
+
+class SucursalAdmin(admin.ModelAdmin):
+    inlines = [TelefonoSucursalInLine,]
+    
+class EmpleadoAdmin(admin.ModelAdmin):
+    list_display=('legajo','nombre','apellido')
+    list_filter=('legajo','apellido')
+    ordering=('legajo',)
+    search_fields=('legajo','nombre','apellido','numero_documento')
+
+admin.site.register(Empleado, EmpleadoAdmin)
+admin.site.register(Sucursal, SucursalAdmin)
 admin.site.register(TipoDocumento)
 admin.site.register(Barrio)
 admin.site.register(Localidad)
