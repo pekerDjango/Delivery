ab87537db4e73716e868533b81b91410a7760304
Federico Peker <federicopeker@gmail.com>
2013-09-10 13:23:34 -0700

    [#11] carrousel andando joya con css/sigep.css

--- PedidoRegistrado/forms.py
+++ PedidoRegistrado/forms.py
@@ -1,10 +1,16 @@
 from django import forms
+from django.forms import Textarea, TextInput
 from PedidoRegistrado.models import DomicilioSearch
 
 class DomicilioSearchForm(forms.ModelForm):
     class Meta:
         model = DomicilioSearch
         fields = ['localidad', 'barrio', 'direccion', 'numero_direccion', 'piso', 'depto', 'codigo_postal']
+        widgets = {
+                  "piso": TextInput(attrs={'width':'40px'}),
+                  "depto":TextInput(attrs={'size':'50'})
+                  }
+#        piso = forms.CharField(widget=forms.TextInput(attrs={'size':'60','maxlength':'70'})) 
 
 
 
