0e1c7d9ddf715db433c83b54905616a598f4d435
gi-c <giselle.carletti@gmail.com>
2013-11-05 12:27:16 -0800

    pag consulta de productos (parcial)

--- ComponentesDePedido/urls.py
+++ ComponentesDePedido/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns,url
 
 urlpatterns = patterns('ComponentesDePedido.views',    
      url(r'^producto/$','productoListado_view',name='vista_producto_listado'),
+    
 
 )   
--- ComponentesDePedido/views.py
+++ ComponentesDePedido/views.py
@@ -5,11 +5,30 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
 
+
 def productoListado_view(request):
-    lista_productos = Producto.objects.all()
-    lista_versiones_productos = DetalleVersiones.objects.all()
-    lista_tipos_productos = TipoProducto.objects.all()
-    ctx = {'lista_productos':lista_productos, 'lista_versiones': lista_versiones_productos,
-            'lista_tipos_productos': lista_tipos_productos}
-    return render_to_response('ComponentesDePedido/ListadoProductos.html',ctx,context_instance=RequestContext(request))
+	lista_productos = Producto.objects.all()
+	print lista_productos
+	nro=1
+	lista=[]
+	for producto in lista_productos:
+    		tipo_producto_imagen= TipoProducto.objects.get(producto=nro).imagen
+    		
+    		tipo_producto_nombre= TipoProducto.objects.get(producto=nro).nombre
+    		
+    		print tipo_producto_nombre
+    		print tipo_producto_imagen
+    		print nro
+    		nro= nro + 1
+
+    		
+    		
+    		
+	ctx={'tipo_producto_imagen':tipo_producto_imagen,'tipo_producto_nombre':tipo_producto_nombre,'lista_productos':lista_productos}
+ 	return render_to_response('ComponentesDePedido/ListadoProductos.html',ctx,context_instance=RequestContext(request))
+		
+    	
+    	
+   		
     
+   	   
